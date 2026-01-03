e4287c6e8ac854290044bc6907c1a8a0d21eefb5
amjundii <amjundii@gmail.com>
2026-01-03 16:00:31 +0800

    Create StudentReg.java

---
+++ src/StudentReg.java
@@ -0,0 +1,11 @@
+public class StudentReg {
+  Student student;
+  String session;
+  int semester;
+
+  StudentReg(Student student, String session, int semester){
+    this.student = student;
+    this.session = session;
+    this.semester = semester;
+  }
+}
