81bd77282b0faa2e0a958b1eb35e6f8afb455307
amjundii <amjundii@gmail.com>
2026-01-03 12:55:03 +0800

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -2,6 +2,8 @@ public class Course {
   String name;
   String code;
   int credits;
+  ArrayList<LecturerAssg> lectAssgList = new ArrayList<>();
+  ArrayList<StudentReg> studRegList = new ArrayList<>();
 
   Course(String name, String code, int credits){
     this.name = name;
@@ -16,4 +18,22 @@ public class Course {
   public int getCredits() {
     return credits;
   }
+
+  /*public void insertlectAssgList() {
+    try (BufferedReader br = new BufferedReader(new FileReader("csv_database/CoursesAssg.csv"))){
+      String line;
+      while ((line = br.readLine()) != null) {
+        // Handle UTF-8 BOM (Byte Order Mark) if present
+        line = line.replace("\uFEFF", "").trim();
+                
+        // Check if line is not empty
+        if (line.isEmpty()) continue;
+                
+        String[] values = line.split(",");
+        if (values.length >= 3) {
+          crsList.add(new Course(values[0].trim(), values[1].trim(), Integer.parseInt(values[2].trim())));
+        }
+      }
+    }
+  }*/
 }
