814b0c0abf7cc467a5365bac165db43c754d2a1a
amjundii <amjundii@gmail.com>
2026-01-03 15:59:07 +0800

    Create LecturerAsgg.java

---
+++ src/LecturerAsgg.java
@@ -0,0 +1,11 @@
+public class LecturerAssg {
+  Lecturer lecturer;
+  String session;
+  int semester;
+
+  LecturerAssg(Lecturer lecturer, String session, int semester){
+    this.lecturer = lecturer;
+    this.session = session;
+    this.semester = semester;
+  }
+}
