a6006d057314cf4736675e563667433f89e9859a
amjundii <amjundii@gmail.com>
2026-01-03 09:18:07 +0800

    Update CourseAssg.java

--- src/CourseAssg.java
+++ src/CourseAssg.java
@@ -1,1 +1,11 @@
+public class CourseAssg {
+  Course course;
+  String session;
+  int semester;
 
+  CourseAssg(Course course, String session, int semester){
+    this.course = coures;
+    this.session = session;
+    this.semester = semeseter;
+  }
+}
